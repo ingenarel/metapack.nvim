c7b3ddceb4485691623d5c97ca6f647beb1680ce
ingenarel <ingenarelitems@gmail.com>
2025-01-31 14:02:07 +0600

    (fix): neovim holdups

--- lua/metapack/init.lua
+++ lua/metapack/init.lua
@@ -3,8 +3,11 @@ local m = {}
 
 ---@return boolean
 ---@param executableName string
-function m._checkinstalled(executableName)
-	if vim.uv.fs_stat("/usr/bin/" .. executableName) or vim.uv.fs_stat("/bin/" .. executableName) then
+function m._checkInPath(executableName)
+	if
+		vim.fn.executable(executableName) == 1
+		or vim.uv.fs_stat(vim.fn.stdpath("data") .. "/mason/bin/" .. executableName)
+	then
 		return true
 	end
 	return false
@@ -28,55 +31,70 @@ function m.ensure_installed(packagesData, doas)
 	for i = 1, #packagesData do
 		if type(packagesData[i]) == "string" then
 			-- emerge --ask n --pretend --oneshot --nodeps --verbose n --color n
-			if
-				string.find(osData.release, "gentoo")
-				and m._checkinstalled(packagesData[i]) == false
-				and vim.system({
-						"emerge",
-						"--ask",
-						"n",
-						"--pretend",
-						"--oneshot",
-						"--nodeps",
-						"--verbose",
-						"n",
-						"--color",
-						"n",
-						packagesData[i],
-					})
-						:wait().code
-					== 0
-			then
-				table.insert(emergePackages, packagesData[i])
-			else
-				table.insert(masonPackages, packagesData[i])
+			-- vim.print(packagesData[i])
+			-- vim.print(m._checkInPath(packagesData[i]))
+			-- print(m._checkInPath(packagesData[i]))
+			if m._checkInPath(packagesData[i]) == false then
+				-- vim.print(packagesData[i] .. " = false")
+				print("searching for " .. packagesData[i])
+				if
+					string.find(osData.release, "gentoo")
+					and vim.system({
+							"emerge",
+							"--ask",
+							"n",
+							"--pretend",
+							"--oneshot",
+							"--nodeps",
+							"--verbose",
+							"n",
+							"--color",
+							"n",
+							packagesData[i],
+						})
+							:wait().code
+						== 0
+				then
+					table.insert(emergePackages, packagesData[i])
+				else
+					table.insert(masonPackages, packagesData[i])
+				end
 			end
+		end
+		-- print("mason")
+		-- vim.print(masonPackages)
+		-- print("emerge")
+		-- vim.print(emergePackages)
+		-- print("\n")
+	end
+
+	---@type string
+	local emergeCommand = ""
+	if #emergePackages > 0 then
+		if doas == true then
+			emergeCommand = emergeCommand .. "doas"
+		else
+			emergeCommand = emergeCommand .. "sudo"
+		end
+		emergeCommand = emergeCommand .. " emerge --ask y --verbose --color y --quiet-build y"
+		for i = 1, #emergePackages do
+			emergeCommand = emergeCommand .. " " .. emergePackages[i]
 		end
 	end
 
 	---@type string
-	local command = ""
-	if #emergePackages > 0 then
-		if doas == true then
-			command = command .. "doas"
-		else
-			command = command .. "sudo"
-		end
-		command = command .. " emerge --ask y --verbose --color y --quiet-build y"
-		for i = 1, #emergePackages do
-			command = command .. " " .. emergePackages[i]
-		end
-	end
-
+	local masonCommand = ""
 	if #masonPackages > 0 then
-		require("mason-tool-installer").setup(masonPackages)
+		-- require("mason-tool-installer").setup({ ensure_installed = masonPackages })
+		for i = 1, #masonPackages do
+			masonCommand = masonCommand .. " " .. masonPackages[i]
+		end
+		-- print(masonCommand)
+		vim.cmd("MasonInstall" .. masonCommand)
 	end
 
-	-- vim.print(masonPackages)
-	-- vim.print(emergePackages)
-
-	if #command > 0 then
-		vim.cmd("bot split|" .. "resize 10|" .. "terminal " .. command)
+	if #emergeCommand > 0 then
+		vim.cmd("bot split|" .. "resize 10|" .. "terminal " .. emergeCommand)
 	end
 end
 
